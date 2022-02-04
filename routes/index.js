4cca5dabc539dfc83230c59c6e6b8ae4f27d3aff
Maia Duek <duekmaia@gmail.com>
2022-02-04 09:32:14 -0500

    styling updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", function (req,res) {
+  Movie.find()
+    .then((results) => {
+      console.log("SUCCESS: ", results)
+      res.render("movies", {movies: results})
+    })
+    .catch((err) => console.log("ERROR::: ", err))
+})
+
+router.get("/movies/:movieId", function (req, res) {
+  Movie.findById(req.params.movieId)
+  .then((results) => {
+    console.log("Movie was found!", results)
+    res.render("about-movie", {
+      title: results.title,
+      director: results.director,
+      stars: results.stars,
+      image: results.image,
+      description: results.description,
+      showtimes: results.showtimes
+    })
+  })
+  .catch((err) => console.log("movie was not found :(", err))
+})
 
 module.exports = router;
